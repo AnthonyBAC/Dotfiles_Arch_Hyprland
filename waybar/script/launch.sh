766da9e12c657af2ec3573fb77cf14a70eef2cb7
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-06-16 23:55:08 -0400

    Packages Update

--- waybar/script/launch.sh
+++ waybar/script/launch.sh
@@ -1,3 +1,9 @@
-pkill waybar
-sleep 0.5
-waybar
+#!/bin/bash
+
+if pgrep -x "waybar" > /dev/null; then
+  # If Waybar is running, kill it
+  pkill waybar
+else
+  # If Waybar is not running, start it
+  waybar &
+fi
